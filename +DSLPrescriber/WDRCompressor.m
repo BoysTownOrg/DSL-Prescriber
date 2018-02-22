fc0c42ffde737ff5aebd4d2fc6e6d4e45f083d7c
sbashford <Seth.Bashford@boystown.org>
2018-02-22 12:08:49 -0600

    merging function

--- +DSLPrescriber/WDRCompressor.m
+++ +DSLPrescriber/WDRCompressor.m
@@ -16,7 +16,7 @@ classdef WDRCompressor < handle
             in_pdB = self.parameters.maxdB + 20.*log10(in_peak);
             CL_TK = 105; % Compression limiter threshold kneepoint
             CL_CR = 10;  % Compression limiter compression ratio
-            in_c = DSLPrescriber.WDRC_Circuit(x,0,in_pdB,CL_TK,CL_CR,CL_TK);
+            in_c = self.WDRC_Circuit(x,0,in_pdB,CL_TK,CL_CR,CL_TK);
             Nchannel = length(self.parameters.TKGain);
             if Nchannel > 1
                 y = DSLPrescriber.HA_fbank2(self.parameters.crossFrequencies,in_c,fs);    % Nchannel FIR filter bank
@@ -36,13 +36,31 @@ classdef WDRCompressor < handle
                 end
                 peak = DSLPrescriber.Smooth_ENV(y(:,n),self.parameters.attackMilliseconds,self.parameters.releaseMilliseconds,fs);
                 pdB(:,n) = self.parameters.maxdB+20.*log10(peak);
-                [c(:,n),gdB(:,n)] = DSLPrescriber.WDRC_Circuit(y(:,n),self.parameters.TKGain(n),pdB(:,n),self.parameters.TK(n),self.parameters.CR(n),self.parameters.BOLT(n));
+                [c(:,n),gdB(:,n)] = self.WDRC_Circuit(y(:,n),self.parameters.TKGain(n),pdB(:,n),self.parameters.TK(n),self.parameters.CR(n),self.parameters.BOLT(n));
             end
             comp=sum(c,2);
             out_peak = DSLPrescriber.Smooth_ENV(comp,1,50,fs);
             out_pdB = self.parameters.maxdB + 20.*log10(out_peak);
-            out_c = DSLPrescriber.WDRC_Circuit(comp,0,out_pdB,CL_TK,CL_CR,CL_TK);
+            out_c = self.WDRC_Circuit(comp,0,out_pdB,CL_TK,CL_CR,CL_TK);
             y = out_c(89:end-88);
+        end
+    end
+    
+    methods (Access = private)
+        function [comp,gdB] = WDRC_Circuit(self, x,TKgain,pdB,TK,CR,BOLT)
+            if TK+TKgain > BOLT
+                TK = BOLT-TKgain; 
+            end
+            TKgain_origin = TKgain + (TK.*(1-1./CR));
+            gdB = ((1./CR)-1).*pdB + TKgain_origin;
+            if CR >= 1
+                gdB(pdB < TK) = TKgain;
+            end
+            pBOLT = CR*(BOLT - TKgain_origin);
+            I = pdB > pBOLT;
+            gdB(I) = BOLT+((pdB(I)-pBOLT)*1/10)-pdB(I);
+            g=10.^(gdB/20);
+            comp=x.*g;
         end
     end
 end
