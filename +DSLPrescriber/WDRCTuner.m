5226a80b5d116bfefac02e5104a4e3722281c20c
sbashford <Seth.Bashford@boystown.org>
2018-02-22 09:58:42 -0600

    merging function

--- +DSLPrescriber/WDRCTuner.m
+++ +DSLPrescriber/WDRCTuner.m
@@ -173,7 +173,7 @@ classdef WDRCTuner < handle
             B = zeros(nF, 7);
             A = zeros(nF, 7);
             for i = nF:-1:10
-                [B(i, :), A(i, :)] = DSLPrescriber.oct3dsgn(ff(i), Fs, N);
+                [B(i, :), A(i, :)] = self.oct3dsgn(ff(i), Fs, N);
             end
             for j = 0:2
                 rowOffset = j * 3;
@@ -208,6 +208,49 @@ classdef WDRCTuner < handle
             for n = 1:nF
                 banana(n) = maxdB + 10*log10((mean(P(:,n))));
             end
+        end
+        
+        function [B, A] = oct3dsgn(self, Fc, Fs, N)
+            % OCT3DSGN  Design of a one-third-octave filter.
+            %    [B,A] = OCT3DSGN(Fc,Fs,N) designs a digital 1/3-octave filter with
+            %    center frequency Fc for sampling frequency Fs.
+            %    The filter is designed according to the Order-N specification
+            %    of the ANSI S1.1-1986 standard. Default value for N is 3.
+            %    Warning: for meaningful design results, center frequency used
+            %    should preferably be in range Fs/200 < Fc < Fs/5.
+            %    Usage of the filter: Y = FILTER(B,A,X).
+            %
+            %    Requires the Signal Processing Toolbox.
+            %
+            %    See also OCT3SPEC, OCTDSGN, OCTSPEC.
+
+            % Author: Christophe Couvreur, Faculte Polytechnique de Mons (Belgium)
+            %         couvreur@thor.fpms.ac.be
+            % Last modification: Aug. 25, 1997, 2:00pm.
+
+            % References:
+            %    [1] ANSI S1.1-1986 (ASA 65-1986): Specifications for
+            %        Octave-Band and Fractional-Octave-Band Analog and
+            %        Digital Filters, 1993.
+
+            if (nargin == 2)
+                N = 3;
+            end
+            if (Fc > 0.88*(Fs/2))
+                error('Design not possible. Check frequencies.');
+            end
+
+            % Design Butterworth 2Nth-order one-third-octave filter
+            % Note: BUTTER is based on a bilinear transformation, as suggested in [1].
+            pi = 3.14159265358979;
+            f1 = Fc/(2^(1/6));
+            f2 = Fc*(2^(1/6));
+            Qr = Fc/(f2-f1);
+            Qd = (pi/2/N)/(sin(pi/2/N))*Qr;
+            alpha = (1 + sqrt(1+4*Qd^2))/2/Qd;
+            W1 = Fc/(Fs/2)/alpha;
+            W2 = Fc/(Fs/2)*alpha;
+            [B, A] = butter(N, [W1, W2]);
         end
     end
 end
