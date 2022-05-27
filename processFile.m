062934375d0cf21785c8b416b7a7a7f84bdc8870
Seth Bashford <seth.bashford@boystown.org>
2022-05-27 14:43:24 -0500

    add input level to processFile

--- processFile.m
+++ processFile.m
@@ -1,18 +1,22 @@
-function processFile(prescriptionFilePath, audioFilePath)
-fid = fopen(prescriptionFilePath);
-raw = fread(fid, inf);
-str = char(raw');
-fclose(fid);
-j = jsondecode(str);
+function processFile(prescriptionFilePath, audioFilePath, inputLevel_dB_SPL)
+json = readJsonFile(prescriptionFilePath);
 p.fullscale_dB_SPL = 119;
-p.BOLT_dB_SPL = j.BOLT_dB_SPL;
-p.kneepointGain_dB = j.kneepoint_gains_dB;
-p.crossFrequenciesHz = j.cross_frequencies_Hz;
-p.attack_ms = j.attack;
-p.release_ms = j.release;
-p.kneepoint_dB_SPL = j.kneepoints_dB_SPL;
-p.ratio = j.compression_ratios;
-h = dslprescriber.HearingAidProcessor(p);
+p.BOLT_dB_SPL = json.BOLT_dB_SPL;
+p.kneepointGain_dB = json.kneepoint_gains_dB;
+p.crossFrequenciesHz = json.cross_frequencies_Hz;
+p.attack_ms = json.attack;
+p.release_ms = json.release;
+p.kneepoint_dB_SPL = json.kneepoints_dB_SPL;
+p.ratio = json.compression_ratios;
+hearingAid = dslprescriber.HearingAidProcessor(p);
 [y, fs] = audioread(audioFilePath);
-audiowrite([audioFilePath(1:end-4), '-hearing-aid-simulated', audioFilePath(end-3:end)], h.process(y, fs), fs)
+outputFilePath = [audioFilePath(1:end-4), '-hearing-aid-simulated', audioFilePath(end-3:end)];
+audiowrite(outputFilePath, hearingAid.process(y * 10^((inputLevel_dB_SPL - p.fullscale_dB_SPL)/20) / rms(y), fs), fs)
+end
+
+function json = readJsonFile(path)
+fid = fopen(path);
+contents = char(fread(fid, inf)');
+fclose(fid);
+json = jsondecode(contents);
 end
