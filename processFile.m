4f52325dfc4ba9428c37612deb22aa95d7610d74
Seth Bashford <seth.bashford@boystown.org>
2022-06-03 11:04:57 -0500

    add input level to output file path in process example

--- processFile.m
+++ processFile.m
@@ -10,7 +10,7 @@ p.kneepoint_dB_SPL = json.kneepoints_dB_SPL;
 p.ratio = json.compression_ratios;
 hearingAid = dslprescriber.HearingAidProcessor(p);
 [y, fs] = audioread(audioFilePath);
-outputFilePath = [audioFilePath(1:end-4), '-hearing-aid-simulated', audioFilePath(end-3:end)];
+outputFilePath = [audioFilePath(1:end-4), '-hearing-aid-simulated-', num2str(inputLevel_dB_SPL), audioFilePath(end-3:end)];
 audiowrite(outputFilePath, hearingAid.process(y * 10^((inputLevel_dB_SPL - p.fullscale_dB_SPL)/20) / rms(y), fs), fs)
 end
 
